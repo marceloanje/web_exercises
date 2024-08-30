26a2fe92d014fcc648572e29c1d5f758007c5914
marceloanje <marcelo.anje@outlook.com>
2024-08-30 19:46:36 -0300

    feat: Add connection with sql

--- guia-perguntas/index.js
+++ guia-perguntas/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!");
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    });
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -16,11 +27,14 @@ app.get("/perguntar", (req, res) => {
 });
 
 app.post("/salvarpergunta", (req, res) => {
-    var titulo = req.body.titulo;
-    var descricao = req.body.descricao;
-    res.send(
-        `Formulário recebido! título: ${titulo} - descrição: ${descricao}`
-    );
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao,
+    }).then(() => {
+        res.redirect("/");
+    });
 });
 
 app.listen(8080, () => {
