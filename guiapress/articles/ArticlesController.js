f8fc63a4febd8b32d032b5f9376fd93736e43f22
marceloanje <marcelo.anje@outlook.com>
2024-09-13 21:53:55 -0300

    feat: Add views to add and display articles, registering them in the database

--- guiapress/articles/ArticlesController.js
+++ guiapress/articles/ArticlesController.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
-    res.send("Router articles");
+router.get("/admin/articles", (req, res) => {
+    Article.findAll().then((articles) => {
+        res.render("admin/articles/index", { articles: articles });
+    });
 });
 
 router.get("/admin/articles/new", (req, res) => {
-    res.send("Router for create a new article");
+    Category.findAll().then((categories) => {
+        res.render("admin/articles/new", { categories: categories });
+    });
+});
+
+router.post("/articles/save", (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let categoryId = req.body.category;
+
+    if (title == undefined) res.redirect("/admin/articles");
+    if (body == undefined) res.redirect("/admin/articles");
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: categoryId,
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
 });
 
 module.exports = router;
