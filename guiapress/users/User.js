c1d65c5aa09d8e097a2d3a2cdb342fa65a10ac8c
marceloanje <marcelo.anje@outlook.com>
2024-09-16 19:17:04 -0300

    feat: Add user creation view

---
+++ guiapress/users/User.js
@@ -0,0 +1,15 @@
+const Sequilize = require("sequelize");
+const connection = require("../database/database");
+
+const User = connection.define("users", {
+    email: {
+        type: Sequilize.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: Sequilize.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = User;
