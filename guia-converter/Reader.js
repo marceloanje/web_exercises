de5d09ce99a608710d6b176511563f2d083bf05d
marceloanje <marcelo.anje@outlook.com>
2024-11-09 22:49:59 -0300

    feat: add class Reader

--- guia-converter/Reader.js
+++ guia-converter/Reader.js
@@ -1,1 +1,12 @@
-class Reader {}
+const fs = require("fs");
+
+class Reader {
+    Read(filepath) {
+        fs.readFile(filepath, "utf-8", (err, data) => {
+            if (err) throw err;
+            console.log(data);
+        });
+    }
+}
+
+module.exports = Reader;
---
+++ guia-converter/app.js
@@ -0,0 +1,5 @@
+let Reader = require("./Reader");
+
+let reader = new Reader();
+
+reader.Read("./users.csv");
