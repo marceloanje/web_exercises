db6daf73eda3972887b3b28f1a5e9b42a53ceb13
marceloanje <marcelo.anje@outlook.com>
2024-11-22 21:24:21 -0300

    feat: add Table class

--- guia-converter/app.js
+++ guia-converter/app.js
@@ -1,12 +1,14 @@
 let Reader = require("./Reader");
 let Processor = require("./Processor");
+let Table = require("./Table");
 
 let reader = new Reader();
 
 async function main() {
     let data = await reader.Read("./users.csv");
-
     let processedData = Processor.Process(data);
+
+    let users = new Table(processedData);
 }
 
 main();
